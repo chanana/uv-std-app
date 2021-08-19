5d1823360337a1d0d82395ac95f4a41c4fe338c7
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2021-08-19 08:47:29 -0400

    Ignore ipynb, make peak finder

---
+++ find_peaks.py
@@ -0,0 +1,22 @@
+from scipy.signal import find_peaks, peak_widths
+
+
+def find_peaks(x, height=None):
+    """Find peaks in given 1D vector
+
+    Args:
+        x (list): 1D vector of the y values of a waveform
+        height ([float], optional): The minimum height required to be considered a
+        peak. If no height is specified, it is taken as 10% of the maximum value in x.
+        Defaults to None.
+    """
+    if not height:
+        height = 0.1 * max(x)
+
+    peaks, heights = find_peaks(x=x, height=height)
+    # fwhm = full width at half max (width of the peak at specified height)
+    # hm = half max (height at which fwhm was found),
+    # leftips, rightips = intersection on x axis for y=hm
+    fwhm, hm, leftips, rightips = peak_widths(x=x, peaks=peaks, rel_height=0.5)
+
+    return {"fwhm": fwhm, "hm": hm, "leftips": leftips, "rightips": rightips}
