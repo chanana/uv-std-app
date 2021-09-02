f7202eaa73e437ce6ae6adff440ae5ab30547290
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2021-09-02 13:44:21 -0400

    Add position threshold based on NPB

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
 # Theme for plotly graphs
 PLOTLY_THEME = "ggplot2"
 
+# Reasonable threshold set for NPB (in seconds)
+THRESHOLD_POSITION = 3
+
 # style_data_condition for alternating row colors
 ALTERNATE_ROW_HIGHLIGHTING = {
     "if": {"row_index": "odd"},
